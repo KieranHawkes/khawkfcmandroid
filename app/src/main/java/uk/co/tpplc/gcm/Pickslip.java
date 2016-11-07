38f37a4d1b518ce4af60582a649ae744bb16f29f
Kieran Hawkes <kieran.hawkes@travisperkins.co.uk>
2016-11-07 11:24:30 +0000

    Clean up code and extract string constants

--- app/src/main/java/uk/co/tpplc/gcm/Pickslip.java
+++ app/src/main/java/uk/co/tpplc/gcm/Pickslip.java
@@ -1,12 +1,10 @@
 package uk.co.tpplc.gcm;
 
-import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -27,16 +25,14 @@ public class Pickslip extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Bundle extras = getIntent().getExtras();
 
-        String pick = extras.getString("PickDets");
-
-
+        String pick = extras.getString(getString(R.string.pickDetailElement));
 
         try {
             JSONObject dets = new JSONObject(pick);
             TableLayout ll = new TableLayout(this);
-            ll.setOrientation(TableLayout.VERTICAL);
+            ll.setOrientation(LinearLayout.HORIZONTAL);
             TableRow.LayoutParams tableRowParams = new TableRow.LayoutParams();
-            tableRowParams.setMargins(8,2,8,2);
+            tableRowParams.setMargins(8, 2, 8, 2);
             TableRow headings = new TableRow(this);
             TextView hdr1 = new TextView(this);
             TextView hdr2 = new TextView(this);
@@ -47,25 +43,25 @@ public class Pickslip extends AppCompatActivity {
             hdr1.setTypeface(null, Typeface.BOLD);
             hdr2.setTypeface(null, Typeface.BOLD);
             hdr3.setTypeface(null, Typeface.BOLD);
-            headings.addView(hdr1,tableRowParams);
-            headings.addView(hdr2,tableRowParams);
-            headings.addView(hdr3,tableRowParams);
-            ll.addView(headings,tableRowParams);
+            headings.addView(hdr1, tableRowParams);
+            headings.addView(hdr2, tableRowParams);
+            headings.addView(hdr3, tableRowParams);
+            ll.addView(headings, tableRowParams);
 
- //           ListView lv = new ListView(this);
-            for (int i = 0; i < dets.getJSONArray("Lines").length(); i++) {
-                JSONObject line = dets.getJSONArray("Lines").getJSONObject(i);
+            //           ListView lv = new ListView(this);
+            for (int i = 0; i < dets.getJSONArray(getString(R.string.LinesElement)).length(); i++) {
+                JSONObject line = dets.getJSONArray(getString(R.string.LinesElement)).getJSONObject(i);
                 TableRow tableRow = new TableRow(this);
                 TextView prodCode = new TextView(this);
-                prodCode.setText(line.get("prodCode").toString());
+                prodCode.setText(line.get(getString(R.string.productCodeElement)).toString());
                 TextView description = new TextView(this);
-                description.setText(line.get("Description").toString());
+                description.setText(line.get(getString(R.string.descriptonElement)).toString());
                 TextView location = new TextView(this);
-                location.setText(line.get("Location").toString());
-                tableRow.addView(prodCode,tableRowParams);
-                tableRow.addView(description,tableRowParams);
-                tableRow.addView(location,tableRowParams);
-                ll.addView(tableRow,tableRowParams);
+                location.setText(line.get(getString(R.string.locationElement)).toString());
+                tableRow.addView(prodCode, tableRowParams);
+                tableRow.addView(description, tableRowParams);
+                tableRow.addView(location, tableRowParams);
+                ll.addView(tableRow, tableRowParams);
 
           /*      StringBuilder text = new StringBuilder();
                 text.append(line.get("LineNo"));
